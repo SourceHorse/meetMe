e01541f7201ebb9a1fb7bdf88f7b23fe1c4278a5
Ross Scarborough <rossscarborough@gmail.com>
2018-12-04 00:18:32 -0500

    Initial commit project setup

---
+++ src/components/googleMap.vue
@@ -0,0 +1,27 @@
+<template>
+    <GmapMap
+        :center="{lat:36, lng:-79}"
+        :zoom="7"
+        map-type-id="terrain"
+        style="width: 500px; height: 300px"
+    >
+        <GmapMarker
+            :key="index"
+            v-for="(m, index) in markers"
+            :position="m.position"
+            :clickable="true"
+            :draggable="true"
+            @click="center=m.position"
+        />
+    </GmapMap>
+</template>
+
+<script>
+    export default {
+        mounted () {
+            this.$refs.mapRef.$mapPromise.then((map) => {
+                map.panTo({lat: 1.38, lng: 103.80})
+            })
+        }
+    }
+</script>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import * as VueGoogleMaps from "vue2-google-maps";
+import {secret} from './config/config.js'
+
+const googleMapsKey = "U2FsdGVkX1+yxj/YsHGc6QLr0MZMZfcMbol596o5GBSab9uYuozpDqCMEnP+bavMotv8fR9HB5M/NN61vElF8g==";
+
+
+var CryptoJS = require("crypto-js");
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: CryptoJS.AES.decrypt(googleMapsKey, secret()).toString(CryptoJS.enc.Utf8),
+    libraries: "places"
+  }
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
