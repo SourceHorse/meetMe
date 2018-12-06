a7b19d0bc7e1b3203f08517375e298f60313c059
Ross Scarborough <rossscarborough@gmail.com>
2018-12-05 23:19:42 -0500

    MM-2 Center on and mark user location

--- src/App.vue
+++ src/App.vue
@@ -2,58 +2,73 @@
   <div id="app">
     <GoogleMap/>
     <PanTool v-on:submit="doPan"/>
+    <MapToolbar v-on:center="centerOnPosition"/>
   </div>
 </template>
 
 <script>
 import GoogleMap from './components/googleMap.vue'
 import PanTool from './components/panTool.vue'
+import MapToolbar from './components/mapToolbar.vue'
+import googleMapVue from './components/googleMap.vue';
+
+var timer;
 
 export default {
-  components: {
-    GoogleMap,
-    PanTool
-  },
-  name: 'app',
-  methods: {
-      doPan: function(lat, long) {
-          GoogleMap.methods.PanTo(lat, long);
-          GoogleMap.methods.SetUserMarker(lat, long);
-      }
-  }
-
+    components: {
+        GoogleMap,
+        PanTool,
+        MapToolbar
+    },
+    name: 'app',
+    mounted() {
+        this.getUserLocation();
+    },
+    methods: {
+        doPan: function(lat, long) {
+            GoogleMap.methods.PanTo(lat, long);
+            GoogleMap.methods.SetUserMarker(lat, long);
+        },
+        centerOnPosition: function() {
+            this.getUserLocation()
+        },
+        getUserLocation() {
+            if (navigator.geolocation){
+                navigator.geolocation.getCurrentPosition(
+                    function success(position) {
+                        if (position.coords
+                            && position.coords.latitude
+                            && position.coords.longitude
+                            && position.timestamp){
+                                GoogleMap.methods.SetUserMarker(position.coords.latitude, position.coords.longitude)
+                                GoogleMap.methods.PanTo(position.coords.latitude, position.coords.longitude)
+                                return latLong;
+                            }
+                        return null
+                    },
+                    function error() {
+                        return null
+                    }
+                )
+            } else {
+                return null
+            }
+        },
+    }
 }
 </script>
 
 <style lang="scss">
 body {
-  margin: 0;
-  padding: 0;
+    margin: 0;
+    padding: 0;
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
--- src/services/userLocation.js
+++
@@ -1,5 +0,0 @@
-function getUserLocation() {
-    navigator.geolocation 
-    ? navigator.geolocation.getCurrentPosition(showPosition)
-    : console.log("Unable to locate user.");
-}
