1cff8ba70156cd38028fbb9ea5b627aa11b3ca7a
Ross Scarborough <rossscarborough@gmail.com>
2018-12-30 02:18:24 -0500

    changed default meetup radius to 5 miles because google maps shits itself

--- src/services/meetupApi.js
+++ src/services/meetupApi.js
@@ -5,23 +5,19 @@ var CryptoJS = require('crypto-js');
 
 const meetupApiKey = 'U2FsdGVkX1+UfPGPY0O+4MaM4huEoqE3r1BxmU0korHx3YtxFlUvAJ3a7R/tnO1VeuG1zlco9YBZcMkpr/n6ng==';
 const baseUrl = "https://api.meetup.com";
-var meetupResults = [];
+const defaultRadiusMiles = 5;
 
 export default {
-    props: {
-        meetups: meetupResults
-    },
     methods: {
         getMeetupsByLocation(lat, long, radius) {
             if(lat && long) {
                 var decryptedKey = CryptoJS.AES.decrypt(meetupApiKey, config.secret()).toString(CryptoJS.enc.Utf8);
-
                 var fetchOptions = {
                     method: 'GET',
                     mode: 'no-cors'
                 }
 
-                return fetchjsonp(`${baseUrl}/2/open_events?lat=${lat}&lon=${long}&key=${decryptedKey}`, fetchOptions)
+                return fetchjsonp(`${baseUrl}/2/open_events?lat=${lat}&lon=${long}&radius=${defaultRadiusMiles}&key=${decryptedKey}`, fetchOptions)
                 .then(function(response) {
                     return response.json()
                   })
